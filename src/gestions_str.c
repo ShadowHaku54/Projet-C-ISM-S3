c1e15e273f0f10908553ad5cf2e984da56f7b9bb
Salim OUEDRAOGO <salim-taoufiq.ouedraogo@ism.edu.sn>
2024-12-22 06:20:49 +0000

    déplacement du contenu de inverseChaine dans gestions_str

--- src/gestions_str.c
+++ src/gestions_str.c
@@ -1,1 +1,14 @@
+#include "gestions_str.h"
+#include <string.h>
 
+void inverseChaine(char *chaine)
+{
+    int N = strlen(chaine);
+
+    int i=0, j=N-1;
+    while(i<j){
+        char tmp = chaine[i];
+        chaine[i++] = chaine[j];
+        chaine[j--] = tmp;
+    }
+}
--- src/gestions_str.h
+++ src/gestions_str.h
@@ -1,1 +1,4 @@
-
+#ifndef __GESTIONS_STR_H__
+#define __GESTIONS_STR_H__
+void inverseChaine(char *chaine);
+#endif // __GESTIONS_STR_H__
